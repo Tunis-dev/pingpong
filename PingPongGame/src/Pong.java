dbcbf754b37e1faa9aa982186832f291e0d2e0f7
BIT504 <tuniscook@gmail.com>
2021-04-29 23:38:16 +1200

    First commit of code to repository

---
+++ PingPongGame/src/Pong.java
@@ -0,0 +1,34 @@
+import javax.swing.JFrame;
+
+public class Pong extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final static String WINDOW_TITLE = "Pong";
+	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 800;
+	
+	public Pong() {
+        setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setResizable(false);        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(new PongPanel());
+        setVisible(true);
+	}
+	
+	public static int getWindowWidth() {
+		return WINDOW_WIDTH;
+	}
+	
+	public static int getWindowHeight() {
+		return WINDOW_HEIGHT;
+	}
+
+	public static void main(String[] args) {
+		new Pong();
+	}
+
+}
